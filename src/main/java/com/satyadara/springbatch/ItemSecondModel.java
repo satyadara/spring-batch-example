032b5591ad5579c0f8b16fbe1e740abcfcbae2c8
Satya Syahputra <a.satya.syahputra@gmail.com>
2020-04-07 11:11:40 +0700

    Add Model #2

---
+++ src/main/java/com/satyadara/springbatch/ItemSecondModel.java
@@ -0,0 +1,23 @@
+package com.satyadara.springbatch;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "item_second_table")
+public class ItemSecondModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private int quantity;
+    private String description;
+}
